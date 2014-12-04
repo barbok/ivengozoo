28da90384d32238eb72934355a6365d0febf91d6
Andrey Urazaev <the.barbok@gmail.Dcom>
2014-12-04 11:40:21 +0400

    mew home and service pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/course', to: 'static_pages#course', via: 'get'
   match '/partner', to: 'static_pages#partner', via: 'get'
+  match '/service', to: 'static_pages#service', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
